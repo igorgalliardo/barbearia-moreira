40047998ae2c5a93638fb72ad12f6c51444ee7db
Igor Galliardo <igorgalliardo@gmail.com>
2026-01-28 20:26:56 -0300

    removing hero and adding slider

--- js/main.js
+++ js/main.js
@@ -68,3 +68,21 @@ const revealOnScroll = new IntersectionObserver(
 revealElements.forEach(el => {
     revealOnScroll.observe(el);
 });
+
+
+/* ========================= */
+/* HERO SLIDER */
+/* ========================= */
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 5000); // troca a cada 5 segundos    
